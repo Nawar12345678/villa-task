8409c804ec386d8cf6262f6d2056f4c52f10c3a2
nawar <nawaralissa23@gmail.com>
2024-07-19 17:51:54 +0200

    first commit

---
+++ src/components/common/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Copyright © 2048 Villa Agency  All rights reserved. Design by: 
+        <a href="#"> Nawar</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
